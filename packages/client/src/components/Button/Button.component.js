f0368a36ad82e1f814ee09c9d7bce6749db361f1
shpomp <karolina.urnieziute@gmail.com>
2023-05-02 21:26:30 +0200

    feat/ small MUI example

--- packages/client/src/components/Button/Button.component.js
+++ packages/client/src/components/Button/Button.component.js
@@ -1,50 +1,16 @@
-import PropTypes from 'prop-types';
+import { Button as MuiButton } from '@mui/material';
 import React from 'react';
-import './Button.styles.css';
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'button--primary' : 'button--secondary';
+export const Button = (props) => {
+  const { label, color, variant } = props;
   return (
-    <button
+    <MuiButton
+      sx={{ margin: '20px' }}
       type="button"
-      className={['button', `button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
+      color={color}
+      variant={variant}
     >
       {label}
-    </button>
+    </MuiButton>
   );
-};
-
-Button.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /**
-   * Button contents
-   */
-  label: PropTypes.string.isRequired,
-  /**
-   * Optional click handler
-   */
-  onClick: PropTypes.func,
-};
-
-Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  size: 'medium',
-  onClick: undefined,
 };
--- packages/client/src/containers/LandingPage/LandingPage.Container.js
+++ packages/client/src/containers/LandingPage/LandingPage.Container.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-console */
+import { Card, CardContent, Chip } from '@mui/material';
+import { blue } from '@mui/material/colors';
 import React, { useEffect, useState } from 'react';
 import { apiURL } from '../../apiURL';
-import './LandingPage.Style.css';
+import { Button } from '../../components/Button/Button.component';
 
 export const LandingPage = () => {
   const [movies, setMovies] = useState([]);
@@ -16,13 +19,25 @@ export const LandingPage = () => {
 
   return (
     <div className="landing-page-container">
-      <span>Landing Page</span>
       {movies.map((movie) => (
-        <div key={movie.id}>
-          <p>{movie.title}</p>
-          <p>{movie.description}</p>
-        </div>
+        <Card sx={{ width: 550, margin: 10 }} variant="outlined" key={movie.id}>
+          <CardContent sx={{ margin: '20px', bgcolor: blue[100] }}>
+            <Chip
+              sx={{ fontSize: '1.2rem' }}
+              color="secondary"
+              label={movie.title}
+            />
+            <p>{movie.description}</p>
+          </CardContent>
+        </Card>
       ))}
+
+      <Button
+        label="click me!"
+        color="primary"
+        variant="contained"
+        onClick={() => console.log('hej!')}
+      />
     </div>
   );
 };
