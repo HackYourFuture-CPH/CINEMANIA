3be9760990e56cad625552cd78c58aa38a452b7b
Dan Jecu <jdanalexandru@gmail.com>
2023-05-08 20:23:45 +0200

    added my movie,crew&members

--- packages/server/seeds/development/5_crew_members.js
+++ packages/server/seeds/development/5_crew_members.js
@@ -7,10 +7,22 @@ exports.seed = async function (knex) {
   await knex('crew_members').del();
   await knex('crew_members').insert([
     {
-      full_name: 'Tom Ellis',
-      birth_date: '1978-11-17',
+      full_name: 'Tom Hardy',
+      birth_date: '1977-09-15',
       image_location:
-        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/zhjIyqpk5bsYgxsh5bSeS6VZtHm.jpg',
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/d81K0RH8UX7tZj49tZaQhZ9ewH.jpg',
+    },
+    {
+      full_name: 'Emily Browning',
+      birth_date: '1988-12-07',
+      image_location:
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/silK3Pdugbf1YOtIdXev1eZxzvF.jpg',
+    },
+    {
+      full_name: 'Chazz Palminteri',
+      birth_date: '1952-05-15',
+      image_location:
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mCbjKVyE5B2tleshbJw44tw3ktZ.jpg',
     },
   ]);
 };
--- packages/server/seeds/development/6_movie_crew.js
+++ packages/server/seeds/development/6_movie_crew.js
@@ -7,5 +7,7 @@ exports.seed = async function (knex) {
   await knex('movie_crew').del();
   await knex('movie_crew').insert([
     { movie_id: 8, crew_member_id: 1, role: 'Actor' },
+    { movie_id: 8, crew_member_id: 2, role: 'Actor' },
+    { movie_id: 8, crew_member_id: 3, role: 'Actor' },
   ]);
 };
