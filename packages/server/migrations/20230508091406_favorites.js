0ec1b3c615aa2ad2459e96708adfcd7e27f19fc3
maccrin <maccrin@gmail.com>
2023-05-08 20:38:58 +0200

    made corerction of created_at column data type

--- packages/server/migrations/20230508091406_favorites.js
+++ packages/server/migrations/20230508091406_favorites.js
@@ -7,7 +7,7 @@ exports.up = function (knex) {
     table.integer('movie_id').unsigned().notNullable();
     table.integer('user_id').unsigned().notNullable();
     table.string('comment');
-    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.datetime('created_at', { precision: 6 }).defaultTo(knex.fn.now(6));
     table.foreign('movie_id').references('movies.id');
     table.foreign('user_id').references('users.id');
     table.primary(['movie_id', 'user_id']);
