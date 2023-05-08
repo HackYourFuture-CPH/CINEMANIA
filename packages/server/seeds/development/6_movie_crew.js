706ada6e6719ed8a6119eccfb6227224196ca659
cygap <113382267+Cygap@users.noreply.github.com>
2023-05-08 12:59:59 +0200

    added empty seed files for crew_members and movie_crew

---
+++ packages/server/seeds/development/6_movie_crew.js
@@ -0,0 +1,11 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('movie_crew').del();
+  await knex('movie_crew').insert([
+    { movie_id: 1, crew_member_id: 1, role: '' },
+  ]);
+};
