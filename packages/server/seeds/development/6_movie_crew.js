cf3eee7eea9a972c9e8c47a942f9f3db65e8217a
Dan Jecu <jdanalexandru@gmail.com>
2023-05-08 21:39:32 +0200

    added mehmet,natalia,vipavee movies

--- packages/server/seeds/development/6_movie_crew.js
+++ packages/server/seeds/development/6_movie_crew.js
@@ -9,5 +9,14 @@ exports.seed = async function (knex) {
     { movie_id: 8, crew_member_id: 1, role: 'Actor' },
     { movie_id: 8, crew_member_id: 2, role: 'Actor' },
     { movie_id: 8, crew_member_id: 3, role: 'Actor' },
+    { movie_id: 9, crew_member_id: 4, role: 'Actor' },
+    { movie_id: 9, crew_member_id: 5, role: 'Actor' },
+    { movie_id: 9, crew_member_id: 6, role: 'Director' },
+    { movie_id: 10, crew_member_id: 7, role: 'Actor' },
+    { movie_id: 10, crew_member_id: 8, role: 'Actor' },
+    { movie_id: 10, crew_member_id: 9, role: 'Actor' },
+    { movie_id: 11, crew_member_id: 10, role: 'Actor' },
+    { movie_id: 11, crew_member_id: 11, role: 'Actor' },
+    { movie_id: 11, crew_member_id: 12, role: 'Actor' },
   ]);
 };
