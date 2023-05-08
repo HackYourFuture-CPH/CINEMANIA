9e2e97cc1e79db017d6596ef6d94331953810cc7
cygap <113382267+Cygap@users.noreply.github.com>
2023-05-09 00:01:18 +0200

    added fetching from the endpoint of all the reviews, calculating average rating on the fly - no backend endpoint yet...

--- packages/client/src/components/MovieView/MovieView.component.js
+++ packages/client/src/components/MovieView/MovieView.component.js
@@ -2,5 +2,5 @@ import React from 'react';
 import RatingStars from '../RatingStars/RatingStars.component';
 
 export default function MovieView(params) {
-  return <RatingStars />;
+  return <RatingStars movieID={4} />;
 }
--- packages/client/src/components/RatingStars/RatingStars.component.js
+++ packages/client/src/components/RatingStars/RatingStars.component.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 
-export default function RatingStars(params) {
+export default function RatingStars({ movieID }) {
+  const [averageRating, setAverageRating] = useState(undefined);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}${process.env.REACT_APP_API_PATH}/movies/${movieID}/reviews`,
+        );
+        if (response.ok) {
+          const data = await response.json();
+          let result = data.reduce((sum, review) => sum + review, 0);
+          result /= data.length;
+          setAverageRating(result);
+        } else {
+          setAverageRating('No reviews found');
+        }
+      } catch (error) {
+        setAverageRating('No rating available');
+      }
+    })();
+  }, [movieID]);
   return (
     <>
-      <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
-      <Typography component="legend">No rating given</Typography>
+      <Rating
+        name="half-rating"
+        defaultValue={2.5}
+        value={isNaN(averageRating) ? 0 : `Rating: ${Number(averageRating)}`}
+        precision={0.5}
+      />
+      <Typography component="legend">
+        {averageRating ?? 'No rating given'}
+      </Typography>
     </>
   );
 }
