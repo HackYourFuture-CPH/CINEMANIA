ad1ebd8d4f3d07fb4de461c5d7241a94b7d7eb76
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2023-05-08 15:18:30 +0200

    Formatted reviews-migration file.

---
+++ packages/server/migrations/20230507222214_reviews.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('reviews', (table) => {
+    table.increments();
+    table.integer('movie_id').unsigned().notNullable();
+    table.foreign('movie_id').references('id').inTable('movies');
+    table.integer('user_id').unsigned().notNullable();
+    table.foreign('user_id').references('id').inTable('users');
+    table.integer('rating').notNullable();
+    table.string('review_text').notNullable();
+    table.datetime('created_at', { precision: 6 }).defaultTo(knex.fn.now(6));
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('reviews');
+};
