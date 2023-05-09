429d105c7d9f57ca9002ec31ca539ae9c64a7392
Mehmet Nuri Cavdar <113375790+mehmetnuricavdar@users.noreply.github.com>
2023-05-09 15:48:55 +0200

    Removed Theme provider since it was in other PR

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -3,25 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { LandingPage } from './containers/LandingPage/LandingPage.Container';
 import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
-
-// MUI THEME
-import { ThemeProvider } from '@mui/material';
-import { theme } from './lib/theme';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div className="app">
-        <Router>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </div>
-    </ThemeProvider>
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
