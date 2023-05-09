6885dece78f3e9d08bb8f287af4baa22d4e5e121
Mehmet Nuri Cavdar <113375790+mehmetnuricavdar@users.noreply.github.com>
2023-05-09 13:23:49 +0200

    feat/FE Footer was created alongside with themeProvide

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -4,16 +4,24 @@ import './App.css';
 import { LandingPage } from './containers/LandingPage/LandingPage.Container';
 import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
 
+// MUI THEME
+import { ThemeProvider } from '@mui/material';
+import { theme } from './lib/theme';
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <div className="app">
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="app">
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
