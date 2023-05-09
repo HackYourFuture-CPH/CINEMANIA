cc18d933b9b473cb4f773151f5cb1cface828289
Dan Jecu <jdanalexandru@gmail.com>
2023-05-09 18:53:18 +0200

    removed the test button

--- packages/client/src/containers/LandingPage/LandingPage.Container.js
+++ packages/client/src/containers/LandingPage/LandingPage.Container.js
@@ -2,7 +2,7 @@ import { Card, CardContent, Chip } from '@mui/material';
 import { blue } from '@mui/material/colors';
 import React, { useEffect, useState } from 'react';
 import { apiURL } from '../../apiURL';
-import { Button } from '../../components/Button/Button.component';
+
 import MovieComponent from '../../components/MovieComponent';
 
 export const LandingPage = () => {
@@ -32,7 +32,6 @@ export const LandingPage = () => {
         </Card>
       ))}
       <MovieComponent />
-      <Button label="click me!" color="primary" variant="contained" />
     </div>
   );
 };
