3f6ae0b016c17b71da9f6eec8a59f09e122b3b22
Kositthai <vkositthai@outlook.com>
2023-05-10 18:23:14 +0200

    Create functions for favorite movies endpoint

--- packages/server/api/controllers/movies.controller.js
+++ packages/server/api/controllers/movies.controller.js
@@ -50,10 +50,39 @@ const createMovie = async (body) => {
   };
 };
 
+// get  favorite movies by userID
+const getFavoriteMovie = async (userID) => {
+  return knex('movies')
+    .select('*')
+    .join('favorites', 'favorites.user_id', '=', 'movies.id')
+    .where({ user_id: userID });
+};
+
+// add favorite movie
+const addFavoriteMovie = async (body) => {
+  await knex('favorites').insert({
+    movie_id: body.movie_id,
+    user_id: body.user_id,
+    comment: body.comment,
+  });
+
+  return {
+    successful: true,
+  };
+};
+
+// remove favorite movie by movieID and userID
+const removeFavoriteMovie = async (movieID, userID) => {
+  return knex('favorites').where({ movie_id: movieID, user_id: userID }).del();
+};
+
 module.exports = {
   getMovies,
   getMovieByID,
   editMovie,
   deleteMovie,
   createMovie,
+  getFavoriteMovie,
+  addFavoriteMovie,
+  removeFavoriteMovie,
 };
