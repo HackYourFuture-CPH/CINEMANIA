69e0688982bde5b1f61c9ec7c074308f12d9634b
Mehmet Nuri Cavdar <113375790+mehmetnuricavdar@users.noreply.github.com>
2023-05-11 14:27:01 +0200

    made changes according to feedback

--- packages/client/src/components/FeaturedMovie.js
+++ packages/client/src/components/FeaturedMovie.js
@@ -8,9 +8,16 @@ export default function FeaturedMovie() {
 
   useEffect(() => {
     async function fetchMovies() {
-      const response = await fetch(`${apiURL()}/movies/featured`);
-      const data = await response.json();
-      setFeaturedMovie(data);
+      try {
+        const response = await fetch(`${apiURL()}/movies/featured`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch featured movie');
+        }
+        const data = await response.json();
+        setFeaturedMovie(data);
+      } catch (error) {
+        return null;
+      }
     }
 
     fetchMovies();
@@ -19,15 +26,26 @@ export default function FeaturedMovie() {
   if (!featuredMovie) {
     return <h4>..Loading</h4>;
   }
+
   return (
-    <Box sx={{ position: 'relative', display: 'flex' }}>
+    <Box
+      sx={{
+        position: 'absolute',
+        display: 'flex',
+        top: '14.875rem',
+        left: '9.25rem',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <Box
         component="img"
         sx={{
           width: '104.5rem',
           height: '49.375rem',
           borderRadius: '24.688rem',
-          objectFit: 'scale-down',
+          // objectFit: 'scale-down', can be used to scale down the horizontal images to fit the container but it doesn't work as
+          // expected since around image is empty space
           background:
             'linear-gradient(180deg, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 51.56%)})',
         }}
@@ -40,24 +58,42 @@ export default function FeaturedMovie() {
           flexDirection: 'column',
           alignItems: 'center',
           position: 'absolute',
-          top: '25%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
+          fontFamily: 'Inter',
+          fontStyle: 'normal',
           textAlign: 'center',
           color: '#fff',
+          top: '3.75rem',
         }}
       >
-        <Typography component="div" variant="h3">
+        <Typography
+          component="div"
+          sx={{
+            fontSize: '4.5rem',
+            fontWeight: 300,
+            lineHeight: '5.446rem',
+          }}
+        >
           Ready for {featuredMovie.title}?
         </Typography>
-        <Box sx={{ display: 'flex', alignItems: 'center', mt: 1, pb: 10 }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
           <IconButton
             sx={{ backgroundColor: '#00FFC2' }}
             aria-label="play/pause"
           >
-            <PlayArrowIcon sx={{ height: 16, width: 16 }} />
+            <PlayArrowIcon sx={{ width: '2.761rem', height: '2.761rem' }} />
           </IconButton>
-          <Typography component="div" variant="h5" sx={{ ml: 1 }}>
+          <Typography
+            component="div"
+            sx={{
+              width: '11.938rem',
+              height: 'fit-content',
+              ml: 1,
+              fontWeight: 500,
+              fontSize: '1.5rem',
+              lineHeight: '1.816rem',
+              textDecorationLine: 'underline',
+            }}
+          >
             Watch the trailer
           </Typography>
         </Box>
