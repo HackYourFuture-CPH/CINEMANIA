c21b03d058e936281ab469d2a19da2773e51736c
maccrin <maccrin@gmail.com>
2023-05-11 11:55:42 +0200

    feat/LIST-10-Movie-list-component

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -4,17 +4,22 @@ import './App.css';
 import { LandingPage } from './containers/LandingPage/LandingPage.Container';
 import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
 import MovieView from './components/MovieView/MovieView.component';
+import MovieListPage from './components/MovieListPage/MovieListPage';
+import { MovieListProvider } from './context/movieListContext';
 
 function App() {
   return (
     <div className="app">
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="movies/:id" element={<MovieView />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Router>
+      <MovieListProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="movies/:id" element={<MovieView />} />
+            <Route path="/movieList" element={<MovieListPage />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Router>
+      </MovieListProvider>
     </div>
   );
 }
