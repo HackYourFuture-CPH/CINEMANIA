01baaba3482a1fbe3a82694727ddfb534250fffc
shobana_mathiarul <mathiarulshobana@gmail.com>
2023-05-13 17:26:36 +0200

    Assembled all the components and fixed some style issues

--- packages/client/src/components/Categories/CategoryButton.jsx
+++ packages/client/src/components/Categories/CategoryButton.jsx
@@ -3,11 +3,17 @@ import { Button as MuiButton } from '@mui/material';
 import { useMovieList } from '../../context/movieListContext';
 
 const CategoryButton = ({ label, categoryId }) => {
-  const { setCategoryId } = useMovieList();
+  const { selectedCategoryId, setSelectedCategoryId } = useMovieList();
 
   const handleClick = () => {
-    setCategoryId(categoryId);
+    if (isActive) {
+      setSelectedCategoryId(null);
+    } else {
+      setSelectedCategoryId(categoryId);
+    }
   };
+
+  const isActive = selectedCategoryId === categoryId;
 
   return (
     <MuiButton
@@ -16,15 +22,15 @@ const CategoryButton = ({ label, categoryId }) => {
         fontWeight: 500,
         fontSize: '16px',
         lineHeight: '10px',
-        color: '#00FFC2',
+        color: isActive ? '#003E2F' : '#00FFC2',
         border: '2px solid #00FFC2',
         borderRadius: '50px',
         padding: '14px 18px',
-        backgroundColor: '#003E2F',
+        backgroundColor: isActive ? '#00FFC2' : '#003E2F',
         textTransform: 'none',
         margin: '10px 6px',
         '&:hover': {
-          backgroundColor: '#FFF',
+          backgroundColor: isActive ? '#00FFC2' : '#FFF',
         },
       }}
       type="button"
--- packages/client/src/components/MovieList/MovieCard.jsx
+++ packages/client/src/components/MovieList/MovieCard.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Card, CardMedia, Typography, Box, Grid } from '@mui/material';
+import { CardMedia, Typography, Box, Grid } from '@mui/material';
 import { Link } from 'react-router-dom';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
@@ -7,79 +7,77 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 const MovieCard = ({ movie }) => {
   return (
     <Grid item xs={12} sm={6} md={4} key={movie.id}>
-      <Card>
-        <Box sx={{ position: 'relative' }}>
-          <Box
+      <Box sx={{ position: 'relative' }}>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundImage:
+              'linear-gradient(360deg, #00FFC2 0%, rgba(0, 0, 0, 0.1) 33%)',
+            order: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-end',
+            justifyContent: 'space-between',
+          }}
+        >
+          {movie.is_favorite ? (
+            <FavoriteIcon
+              sx={{
+                width: '1.9rem',
+                height: '1.75rem',
+                color: '#FF0000',
+                padding: '1rem',
+                backgroundColor: '#000000CC',
+                '&:hover': {
+                  color: '#FF0000',
+                  cursor: 'pointer',
+                },
+              }}
+            />
+          ) : (
+            <FavoriteBorderIcon
+              sx={{
+                width: '1.9rem',
+                height: '1.75rem',
+                color: '#FFFFFF',
+                padding: '1rem',
+                backgroundColor: '#000000CC',
+                '&:hover': {
+                  color: '#FF0000',
+                  cursor: 'pointer',
+                },
+              }}
+            />
+          )}
+          <Typography
+            component={Link}
+            to={`/movies/${movie.id}`}
             sx={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
+              color: '#FFFFFF',
               width: '100%',
-              height: '100%',
-              backgroundImage:
-                'linear-gradient(360deg, #00FFC2 0%, rgba(0, 0, 0, 0.1) 33%)',
-              order: 1,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'flex-end',
-              justifyContent: 'space-between',
+              height: 30,
+              marginBottom: '1.5rem',
+              textAlign: 'center',
+              fontWeight: '500',
+              fontSize: '25px',
+              lineHeight: '30px',
+              textDecoration: 'none',
             }}
           >
-            {movie.isFavorite ? (
-              <FavoriteIcon
-                sx={{
-                  width: '1.9rem',
-                  height: '1.75rem',
-                  color: '#FF0000',
-                  padding: '1rem',
-                  backgroundColor: '#000000CC',
-                  '&:hover': {
-                    color: '#FF0000',
-                    cursor: 'pointer',
-                  },
-                }}
-              />
-            ) : (
-              <FavoriteBorderIcon
-                sx={{
-                  width: '1.9rem',
-                  height: '1.75rem',
-                  color: '#FFFFFF',
-                  padding: '1rem',
-                  backgroundColor: '#000000CC',
-                  '&:hover': {
-                    color: '#FF0000',
-                    cursor: 'pointer',
-                  },
-                }}
-              />
-            )}
-            <Typography
-              component={Link}
-              to={`/movies/${movie.id}`} // replace with your desired route for movie details page
-              sx={{
-                color: '#FFFFFF',
-                width: '100%',
-                height: 30,
-                marginBottom: '1.5rem',
-                textAlign: 'center',
-                fontWeight: '500',
-                fontSize: '25px',
-                lineHeight: '30px',
-                textDecoration: 'none',
-              }}
-            >
-              See details...
-            </Typography>
-          </Box>
-          <CardMedia
-            component="img"
-            image={`${movie.image_location}`}
-            alt={movie.title}
-            sx={{ order: 2, aspectRatio: '414/623' }}
-          />
+            See details...
+          </Typography>
         </Box>
-      </Card>
+        <CardMedia
+          component="img"
+          image={`${movie.image_location}`}
+          alt={movie.title}
+          sx={{ order: 2, aspectRatio: '414/623' }}
+        />
+      </Box>
     </Grid>
   );
 };
