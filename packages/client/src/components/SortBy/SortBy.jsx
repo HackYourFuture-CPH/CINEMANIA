7d81657a691729c3ce8cd5edea450aec26393780
VioMNE <violeta.pavetic@gmail.com>
2023-05-13 21:26:42 +0200

    feat/created sortby component

---
+++ packages/client/src/components/SortBy/SortBy.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@mui/material/styles';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    position: 'absolute',
+    right: 0,
+    color: '#00FFC2',
+  },
+  label: {
+    color: '#00FFC2',
+  },
+  select: {
+    color: '#00FFC2',
+    '&:before': {
+      borderColor: '#00FFC2',
+    },
+    '&:after': {
+      borderColor: '#00FFC2',
+    },
+    '&:hover:not(.Mui-disabled):before': {
+      borderColor: '#00FFC2',
+    },
+  },
+  icon: {
+    fill: '#00FFC2',
+  },
+}));
+
+const SortBy = ({ onChange }) => {
+  // @ts-ignore
+  const classes = useStyles();
+  const [sortBy, setSortBy] = useState('');
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setSortBy(value);
+    onChange(value);
+  };
+
+  return (
+    <FormControl className={classes.formControl}>
+      <InputLabel className={classes.label} id="sort-by-label">
+        Sort By
+      </InputLabel>
+      <Select
+        labelId="sort-by-label"
+        id="sort-by-select"
+        value={sortBy}
+        onChange={handleChange}
+        className={classes.select}
+        inputProps={{
+          classes: {
+            icon: classes.icon,
+          },
+        }}
+      >
+        <MenuItem value="rating">Rating</MenuItem>
+        <MenuItem value="recent">Recently Added</MenuItem>
+        <MenuItem value="price">Price</MenuItem>
+      </Select>
+    </FormControl>
+  );
+};
+
+export default SortBy;
