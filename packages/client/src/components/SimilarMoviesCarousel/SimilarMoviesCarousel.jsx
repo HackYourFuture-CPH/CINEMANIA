2d7185b2f35ad88f76bf606f1307b260e12872f2
cygap <113382267+Cygap@users.noreply.github.com>
2023-05-14 10:54:20 +0200

    gathered all unfinished work together to chowcase how it might look after the approval of our PRs

--- packages/client/src/components/SimilarMoviesCarousel/SimilarMoviesCarousel.jsx
+++ packages/client/src/components/SimilarMoviesCarousel/SimilarMoviesCarousel.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
-import { Container } from '@mui/material';
+import { Container, IconButton } from '@mui/material';
+import './Carousel.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
+import movies from './mock.json';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-const SimilarMoviesCarousel = ({ movieID }) => {
+const SimilarMoviesCarousel = ({ categoryID }) => {
+  // const { SimilarMovies, setSimilarMovies } = useState([]);
+  // useEffect(
+  //   () => {
+  //         fetch(`${apiURL()}/movies/category/${categoryID}`)
+  //           .then((res) => res.json())
+  //           .then((data) => {
+  //             setSimilarMovies(data);
+  //           });
+  //       })
+  //       .catch((error) => {
+  //         throw error.message;
+  //       });
+  //   },
+  //   [categoryID]
+  // );
+  const settings = {
+    className: 'center',
+    infinite: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    arrows: false,
+  };
+  const slider = React.useRef(null);
   return (
     <Container
       sx={{
@@ -13,7 +45,26 @@ const SimilarMoviesCarousel = ({ movieID }) => {
         top: 2000,
       }}
     >
-      SimilarMoviesCarousel
+      <h3 className="carousel-title">| SIMILAR MOVIES</h3>
+      <div style={{ display: 'flex' }}>
+        <IconButton onClick={() => slider?.current?.slickPrev()}>
+          <ArrowBackIosIcon style={{ fill: '#00FFC2' }} fontSize="large" />
+        </IconButton>
+        <div className="view-carousel">
+          <Slider {...settings} ref={slider}>
+            {movies.map((item) => (
+              <div key={item.description} className="box">
+                <a href="https://github.com/HackYourFuture-CPH/CINEMANIA">
+                  <img src={item.image_location} alt={item.title} />
+                </a>
+              </div>
+            ))}
+          </Slider>
+        </div>
+        <IconButton onClick={() => slider?.current?.slickNext()}>
+          <ArrowForwardIosIcon style={{ fill: '#00FFC2' }} fontSize="large" />
+        </IconButton>
+      </div>
     </Container>
   );
 };
