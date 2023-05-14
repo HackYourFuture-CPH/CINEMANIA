eb72cbf3c06c33ece3f55950e17b07d4c42b5752
shpomp <karolina.urnieziute@gmail.com>
2023-05-14 17:37:58 +0200

    styles/ central background gradient

--- packages/client/src/containers/MovieDetails/MovieDetails.jsx
+++ packages/client/src/containers/MovieDetails/MovieDetails.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import './MovieDetails.Style.css';
 import { Container } from '@mui/material';
-import BigMovieCard from '../../components/BigMovieCard/BigMovieCard';
-import TopCastDisplay from '../../components/TopCastDisplay/TopCastDisplay';
-import SimilarMoviesCarousel from '../../components/SimilarMoviesCarousel/SimilarMoviesCarousel';
+import React from 'react';
 import { useParams } from 'react-router-dom';
+import BigMovieCard from '../../components/BigMovieCard/BigMovieCard';
+import SimilarMoviesCarousel from '../../components/SimilarMoviesCarousel/SimilarMoviesCarousel';
+import TopCastDisplay from '../../components/TopCastDisplay/TopCastDisplay';
 
 export const MovieDetails = () => {
   const movieID = useParams().id;
