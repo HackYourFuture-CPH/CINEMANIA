b5d6c7c84238d700b4eeaca89e7507d3a69bdb59
Mehmet Nuri Cavdar <113375790+mehmetnuricavdar@users.noreply.github.com>
2023-05-15 23:06:45 +0200

    optimised MovieCard with emotion/styled

--- packages/client/src/components/MovieCard/MovieCard.jsx
+++ packages/client/src/components/MovieCard/MovieCard.jsx
@@ -1,35 +1,105 @@
+import * as React from 'react';
+import { Card, CardMedia, Typography, Rating, Box } from '@mui/material';
+import styled from '@emotion/styled';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import { Box, Card, CardMedia, Rating, Typography } from '@mui/material';
-import * as React from 'react';
 import { useFavorites } from './useFavorites'; // created a custom hook to manage favorites
 
 const BasicRating = () => {
-  return (
-    <Rating
-      name="read-only"
-      value={4}
-      readOnly
-      sx={{
-        position: 'relative',
-        left: '1.96rem',
-        borderRadius: '0.59',
-        maxHeight: 'min-content',
-        width: '3rem',
-        top: '2rem',
-        paddingBottom: '4rem',
-        color: '#003E2F',
-      }}
-    />
-  );
+  const StyledRating = styled(Rating)({
+    position: 'relative',
+    left: '1.96rem',
+    borderRadius: '0.59rem',
+    maxHeight: 'min-content',
+    width: '3rem',
+    top: '2rem',
+    paddingBottom: '4rem',
+    color: '#003E2F',
+  });
+  return <StyledRating name="read-only" value={4} readOnly />;
 };
 
-export const MovieCard = ({ movie }) => {
+export function MovieCard({ movie }) {
   const [favorites, toggleFavorite] = useFavorites([]);
   const isFavorite = favorites.includes(movie);
 
+  const StyledCard = styled(Card)`
+    display: flex;
+    flex-direction: column;
+    max-height: min-content;
+    width: 17.43rem;
+    left: 6rem;
+    background: linear-gradient(
+      166.18deg,
+      rgba(0, 0, 0, 0.4) 0%,
+      rgba(0, 0, 0, 0) 98.81%
+    );
+    box-shadow: 20px 40px 70px rgba(0, 0, 0, 0.25);
+  `;
+
+  const sharedIconStyles = `
+  display: block;
+  position: absolute;
+  width: 1.9rem;
+  height: 1.75rem;
+  top: 2.25rem;
+  right: 1.5rem;
+  color: #ff0000;
+  padding: 1rem;
+  background-color: #000000cc;
+  z-index: 1;
+  &:hover: {
+    color: #ff0000;
+    cursor: pointer;
+  }
+`;
+
+  const StyledFavoriteIcon = styled(FavoriteIcon)`
+    ${sharedIconStyles}
+  `;
+
+  const StyledFavoriteBorderIcon = styled(FavoriteBorderIcon)`
+    ${sharedIconStyles}
+  `;
+  const StyledCardMedia = styled(CardMedia)`
+    position: relative;
+    max-height: min-content;
+    width: 13.5rem;
+    left: 0;
+    top: 0;
+    padding-top: 2.25rem;
+    padding-left: 1.96rem;
+  `;
+
+  const StyledTypographyTitle = styled(Typography)`
+    position: relative;
+    max-height: min-content;
+    width: 13.45rem;
+    top: 1.5rem;
+    left: 1.96rem;
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 1.172rem;
+    line-height: 1.43rem;
+    color: #003c2d;
+  `;
+
+  const StyledTypographyDescription = styled(Typography)`
+    position: relative;
+    min-height: fit-content;
+    width: 13.43rem;
+    top: 2rem;
+    font-family: Inter;
+    font-weight: 400;
+    left: 1.96rem;
+    font-size: 1.17rem;
+    color: #003c2d;
+    line-height: 1.43rem;
+  `;
+
   return (
-    <Card
+    <StyledCard
       sx={{
         display: 'flex',
         flexDirection: 'column',
@@ -47,48 +117,12 @@ export const MovieCard = ({ movie }) => {
         }}
       >
         {isFavorite ? (
-          <FavoriteIcon
-            sx={{
-              display: 'block',
-              position: 'absolute',
-              width: '1.9rem',
-              height: '1.75rem',
-              top: '2.25rem',
-              right: '1.96rem',
-              color: 'hoverRed',
-              padding: '1rem',
-              backgroundColor: '#000000CC',
-              zIndex: 1,
-              '&:hover': {
-                color: 'hoverRed',
-                cursor: 'pointer',
-              },
-            }}
-            onClick={() => toggleFavorite(movie)}
-          />
+          <StyledFavoriteIcon onClick={() => toggleFavorite(movie)} />
         ) : (
-          <FavoriteBorderIcon
-            sx={{
-              display: 'block',
-              position: 'absolute',
-              width: '1.9rem',
-              height: '1.75rem',
-              top: '2.25rem',
-              right: '1.96rem',
-              color: '#FFFFFF',
-              padding: '1rem',
-              backgroundColor: '#000000CC',
-              zIndex: 1,
-              '&:hover': {
-                color: 'hoverRed',
-                cursor: 'pointer',
-              },
-            }}
-            onClick={() => toggleFavorite(movie)}
-          />
+          <StyledFavoriteBorderIcon onClick={() => toggleFavorite(movie)} />
         )}
       </Box>
-      <CardMedia
+      <StyledCardMedia
         component="img"
         sx={{
           position: 'relative',
@@ -102,43 +136,12 @@ export const MovieCard = ({ movie }) => {
         image={movie.image_location}
         alt={movie.title}
       />
-      <Typography
-        sx={{
-          position: 'relative',
-          maxHeight: 'min-content',
-          width: '13.45rem',
-          top: '1.5rem',
-          left: '1.96rem',
-          fontFamily: 'Inter',
-          fontStyle: 'normal',
-          fontWeight: 700,
-          fontSize: '1.172rem',
-          fontHeight: '1.43rem',
-          color: '#003C2D',
-        }}
-        variant="h4"
-      >
-        {movie.title}
-      </Typography>
+      <StyledTypographyTitle>{movie.title}</StyledTypographyTitle>
 
-      <Typography
-        paragraph
-        sx={{
-          position: 'relative',
-          minHeight: 'fit-content',
-          width: '13.43rem',
-          top: '2rem',
-          fontFamily: 'Inter',
-          fontWeight: 400,
-          left: '1.96rem',
-          fontSize: '1.17rem',
-          color: '#003C2D',
-          lineHeight: '1.43rem',
-        }}
-      >
+      <StyledTypographyDescription paragraph>
         {movie.description}
-      </Typography>
+      </StyledTypographyDescription>
       <BasicRating />
-    </Card>
+    </StyledCard>
   );
-};
+}
