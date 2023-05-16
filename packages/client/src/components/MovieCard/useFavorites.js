9fa017bfb14792e52195b51b7b44fded3f56aee5
Kositthai <vkositthai@outlook.com>
2023-05-16 22:25:30 +0200

    edited toggleFavorite function to handler add and remove favorite movie

--- packages/client/src/components/MovieCard/useFavorites.js
+++ packages/client/src/components/MovieCard/useFavorites.js
@@ -1,13 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { apiURL } from '../../apiURL';
+import { handleAddFavorite, handleRemoveFavorite } from './handlerFavorite';
 
 export const useFavorites = (initialFavorites = []) => {
   const [favorites, setFavorites] = useState(initialFavorites);
+  const userId = 1; // placeholder userId
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    const getFavoriteMovies = async () => {
+      try {
+        const response = await fetch(`${apiURL()}/favorites/${userId}`, {
+          signal: abortController.signal,
+        });
+        const data = await response.json();
+        setFavorites(data);
+      } catch (error) {
+        return error;
+      }
+    };
+    getFavoriteMovies();
+    return () => {
+      abortController.abort();
+    };
+  }, []);
 
   const toggleFavorite = (item) => {
-    if (favorites.includes(item)) {
-      setFavorites(favorites.filter((fav) => fav !== item));
+    const foundMovie = favorites.find((movie) => movie.id === item.id);
+
+    if (foundMovie) {
+      handleRemoveFavorite(item.id, item, favorites, setFavorites, userId);
     } else {
-      setFavorites([...favorites, item]);
+      handleAddFavorite(item.id, item, favorites, setFavorites, userId);
     }
   };
 
