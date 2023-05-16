08bff9bee53d0cc3ca225bf9534a8cc504501c15
maccrin <maccrin@gmail.com>
2023-05-16 15:34:43 +0200

    implement infinite scroll feature in movielist page

--- packages/client/src/components/MovieList/MovieList.jsx
+++ packages/client/src/components/MovieList/MovieList.jsx
@@ -1,10 +1,27 @@
 import { Grid } from '@mui/material';
-import React from 'react';
+import { React, useCallback, useEffect, useState } from 'react';
 import { useMovieList } from '../../context/movieListContext';
 import { MovieCard } from './MovieCard';
 
 export const MovieList = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [displayMovies, setDisplayMovies] = useState(3);
   const { movies = [] } = useMovieList();
+  const handleScroll = useCallback(() => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop >
+      document.documentElement.offsetHeight
+    ) {
+      setIsLoading(true);
+      setDisplayMovies(displayMovies + 3);
+    } else {
+      setIsLoading(false);
+    }
+  }, [displayMovies]);
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [handleScroll]);
   return (
     <Grid
       container
@@ -13,9 +30,10 @@ export const MovieList = () => {
         marginBottom: '3rem',
       }}
     >
-      {movies.map((movie) => (
+      {movies.slice(0, displayMovies).map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
+      {isLoading && <h1>...Loading</h1>}
     </Grid>
   );
 };
