152a2409300ac5c102f3b17635a09f62d97e3582
Alaa Abdelbaki <a.eldeen91@gmail.com>
2023-05-16 16:47:30 +0200

    add endpoint to retrieve reviews details of a movie by its id

--- packages/server/api/controllers/reviews.controller.js
+++ packages/server/api/controllers/reviews.controller.js
@@ -1,4 +1,5 @@
 const knex = require('../../config/db');
+const HttpError = require('../lib/utils/http-error');
 
 const getLatestRatedMovies = async () => {
   return knex('reviews')
@@ -14,6 +15,29 @@ const getLatestRatedMovies = async () => {
     .limit(8);
 };
 
+const getReviewsOfMovieByID = async (id) => {
+  if (isNaN(id)) {
+    throw new HttpError('ID should be a number', 400);
+  }
+  try {
+    const reviews = await knex('reviews')
+      .select(
+        'reviews.id',
+        'users.full_name',
+        'reviews.rating',
+        'reviews.review_text',
+        'reviews.created_at',
+      )
+      .join('users', 'users.id', '=', 'reviews.user_id')
+      .where('reviews.movie_id', id)
+      .orderBy('reviews.created_at', 'desc');
+    return reviews;
+  } catch (error) {
+    throw new HttpError(error.message, 500);
+  }
+};
+
 module.exports = {
   getLatestRatedMovies,
+  getReviewsOfMovieByID,
 };
--- packages/server/api/routes/reviews.router.js
+++ packages/server/api/routes/reviews.router.js
@@ -5,6 +5,17 @@ const router = express.Router({ mergeParams: true });
 // controllers
 const reviewsController = require('../controllers/reviews.controller');
 
+router.get('/movie/:id', (req, res) => {
+  const { id } = req.params;
+  reviewsController
+    .getReviewsOfMovieByID(id)
+    .then((result) => res.json(result))
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+      throw error;
+    });
+});
+
 router.get('/rating/latest', (req, res) => {
   reviewsController
     .getLatestRatedMovies()
