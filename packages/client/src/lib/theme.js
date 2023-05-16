5851d1a1d85850eee848409c7ca21574da5754fa
cygap <113382267+Cygap@users.noreply.github.com>
2023-05-16 19:06:04 +0200

    fixed minor styling issues

--- packages/client/src/lib/theme.js
+++ packages/client/src/lib/theme.js
@@ -4,6 +4,7 @@ export const theme = createTheme({
   palette: {
     mainGreen: '#00FFC2',
     hoverRed: '#FF4000',
+    white: '#FFFFFF',
   },
   typography: {
     fontFamily: "'Inter', sans-serif",
