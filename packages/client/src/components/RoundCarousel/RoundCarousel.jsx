ed90f89d377c21e40cae5016ac0e839859914ee1
YF91925352 <zyf91925352@outlook.com>
2023-05-17 00:49:27 +0200

    fix the styles of Carousel and make it responsive

--- packages/client/src/components/RoundCarousel/RoundCarousel.jsx
+++ packages/client/src/components/RoundCarousel/RoundCarousel.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, IconButton } from '@mui/material';
+import { Box, Container, IconButton, Typography, Link } from '@mui/material';
 import './Carousel.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -16,42 +16,96 @@ const RoundCarousel = ({ carouselData }) => {
     slidesToScroll: 1,
     swipeToSlide: true,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   const slider = React.useRef(null);
   return (
-    <Container
-      sx={{
-        width: 1518,
-        height: 300,
-        disableGutters: true,
-        position: 'absolute',
-        top: 2000,
-      }}
-    >
-      <h3 className="carousel-title">| SIMILAR MOVIES</h3>
-      <div style={{ display: 'flex' }}>
+    <Container>
+      <Typography
+        variant="overline"
+        sx={{
+          height: 30,
+          fontSize: 32,
+          fontWeight: 800,
+          color: '#FFFFFF',
+          borderLeft: 4,
+          p: 1,
+        }}
+      >
+        SIMILAR MOVIES
+      </Typography>
+
+      <Box
+        sx={{
+          display: 'flex',
+          p: 2,
+        }}
+      >
         <IconButton onClick={() => slider?.current?.slickPrev()}>
-          <ArrowBackIosIcon style={{ fill: '#00FFC2' }} fontSize="large" />
+          <ArrowBackIosIcon sx={{ fill: '#00FFC2', fontSize: 40 }} />
         </IconButton>
-        <div className="view-carousel">
+        <Box sx={{ height: '100%', width: '100%' }}>
           <Slider {...settings} ref={slider}>
             {carouselData?.map((item) => (
-              <div key={item.description} className="box">
-                <a href={`/movies/${item.id}`}>
-                  <img
-                    className="carousel-img"
+              <Box
+                key={item.description}
+                sx={{
+                  height: 200,
+                  borderRadius: '120px',
+                  // bgcolor: 'red',
+                  overflow: 'hidden',
+                  position: 'relative',
+                }}
+              >
+                <Link href={`/movies/${item.id}`} underline="none">
+                  <Box
+                    component="img"
+                    sx={{
+                      width: 400,
+                      boxShadow: 5,
+                      position: 'absolute',
+                      top: -170,
+                      left: -10,
+                    }}
                     src={item.image_location}
                     alt={item.title}
                   />
-                </a>
-              </div>
+                </Link>
+              </Box>
             ))}
           </Slider>
-        </div>
+        </Box>
         <IconButton onClick={() => slider?.current?.slickNext()}>
-          <ArrowForwardIosIcon style={{ fill: '#00FFC2' }} fontSize="large" />
+          <ArrowForwardIosIcon
+            style={{ fill: '#00FFC2', fontSize: 40, mb: 20 }}
+          />
         </IconButton>
-      </div>
+      </Box>
     </Container>
   );
 };
