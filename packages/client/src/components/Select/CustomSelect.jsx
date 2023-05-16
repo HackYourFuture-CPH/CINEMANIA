f6975d81cdd4645e644fa153a37e65cdd6b9b13a
VioMNE <violeta.pavetic@gmail.com>
2023-05-16 21:54:58 +0200

    feat/ sortby componenet

---
+++ packages/client/src/components/Select/CustomSelect.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+
+const CustomSelect = ({ onChange, sortBy }) => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 85 }}>
+        <InputLabel
+          id="sort-by-label"
+          style={{ color: '#00FFC2', borderBottom: '1px solid #00FFC2' }}
+        >
+          Sort by
+        </InputLabel>
+        <Select
+          labelId="sort-by-label"
+          id="sort-by-select"
+          name="sortBy"
+          onChange={onChange}
+          label="Sort by"
+          sx={{
+            color: '#00FFC2',
+            '& .MuiSelect-icon': {
+              color: '#00FFC2',
+              fontSize: '2.25rem',
+            },
+            '&:before, &:after': {
+              border: 'none !important',
+            },
+          }}
+        >
+          <MenuItem value="rating">Rating</MenuItem>
+          <MenuItem value="recently_added">Recently added</MenuItem>
+          <MenuItem value="price">Price</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+};
+export { CustomSelect };
