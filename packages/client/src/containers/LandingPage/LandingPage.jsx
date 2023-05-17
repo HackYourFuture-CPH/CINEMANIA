dcb8b1c6fa5db8ed003c6f2b1090f3ffdf983735
Kositthai <vkositthai@outlook.com>
2023-05-17 03:36:00 +0200

    Added ellipse carousel most popular movies inside landing page

--- packages/client/src/containers/LandingPage/LandingPage.jsx
+++ packages/client/src/containers/LandingPage/LandingPage.jsx
@@ -2,11 +2,13 @@ import React /* , { useEffect, useState } */ from 'react';
 /* import { apiURL } from '../../apiURL'; */ // Not Used
 import { FeaturedMovie } from '../../components/FeaturedMovie/FeaturedMovie';
 import { LatestRatingMovies } from '../../components/LatestRatingMovies/LatestRatingMovies';
+import { EllipseCarousel } from '../../components/EllipseCarousel/EllipseCarousel';
 
 export const LandingPage = () => {
   return (
     <div>
       <FeaturedMovie />
+      <EllipseCarousel />
       <LatestRatingMovies />
     </div>
   );
