ca03276352037643b50a769e60ecea2b4b947ccf
shobana_mathiarul <mathiarulshobana@gmail.com>
2023-05-17 11:55:48 +0200

    Revert "implement infinite scroll feature in movielist page"
    This reverts commit 08bff9bee53d0cc3ca225bf9534a8cc504501c15.

--- packages/client/src/components/MovieList/MovieList.jsx
+++ packages/client/src/components/MovieList/MovieList.jsx
@@ -1,27 +1,10 @@
 import { Grid } from '@mui/material';
-import { React, useCallback, useEffect, useState } from 'react';
+import React from 'react';
 import { useMovieList } from '../../context/movieListContext';
 import { MovieCard } from './MovieCard';
 
 export const MovieList = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [displayMovies, setDisplayMovies] = useState(3);
   const { movies = [] } = useMovieList();
-  const handleScroll = useCallback(() => {
-    if (
-      window.innerHeight + document.documentElement.scrollTop >
-      document.documentElement.offsetHeight
-    ) {
-      setIsLoading(true);
-      setDisplayMovies(displayMovies + 3);
-    } else {
-      setIsLoading(false);
-    }
-  }, [displayMovies]);
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [handleScroll]);
   return (
     <Grid
       container
@@ -30,10 +13,9 @@ export const MovieList = () => {
         marginBottom: '3rem',
       }}
     >
-      {movies.slice(0, displayMovies).map((movie) => (
+      {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
-      {isLoading && <h1>...Loading</h1>}
     </Grid>
   );
 };
