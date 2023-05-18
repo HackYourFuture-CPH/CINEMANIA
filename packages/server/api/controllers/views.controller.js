007015bcaf181d61f63b1deeaf76b3a8accfcea6
Kositthai <vkositthai@outlook.com>
2023-05-18 03:33:23 +0200

    Edit query by adding backdrop_URL and movies_id and category

--- packages/server/api/controllers/views.controller.js
+++ packages/server/api/controllers/views.controller.js
@@ -3,14 +3,17 @@ const knex = require('../../config/db');
 const getMostPopularMovies = async () => {
   return knex('views')
     .select(
+      'movies.id',
       'movies.title',
       'movies.description',
       'movies.movie_year',
-      'movies.image_location',
+      'movies.backdrop_URL',
       'movies.price',
+      'categories.name as category',
     )
     .count('views.movie_id as view_count')
     .leftJoin('movies', 'movies.id', 'views.movie_id')
+    .join('categories', 'categories.id', 'movies.category_id')
     .groupBy('movies.id')
     .orderBy('view_count', 'desc')
     .limit(12);
