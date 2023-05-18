7b191c80284b522747383575fb00d39432e1c13f
cygap <113382267+Cygap@users.noreply.github.com>
2023-05-18 11:53:49 +0200

    removed additional container from carousel, set max-width to xl

--- packages/client/src/components/RoundCarousel/RoundCarousel.jsx
+++ packages/client/src/components/RoundCarousel/RoundCarousel.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, IconButton, Typography, Link, Container } from '@mui/material';
+import { Box, IconButton, Typography, Link } from '@mui/material';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
@@ -46,10 +46,10 @@ const RoundCarousel = ({ carouselData }) => {
   };
 
   return (
-    <Container>
+    <>
       <Title>SIMILAR MOVIES</Title>
 
-      <CarouselWrapper>
+      <CarouselWrapper maxWidth="xl">
         <IconButton onClick={() => slider?.current?.slickPrev()}>
           <LeftArrow />
         </IconButton>
@@ -72,7 +72,7 @@ const RoundCarousel = ({ carouselData }) => {
           <RightArrow />
         </IconButton>
       </CarouselWrapper>
-    </Container>
+    </>
   );
 };
 
