a7dbba0b70d55c3fbc6facb2b2320a8c85e89611
VioMNE <violeta.pavetic@gmail.com>
2023-05-19 20:46:25 +0200

    styling categories with @emotion/styled

--- packages/client/src/components/Categories/CategoryButton.jsx
+++ packages/client/src/components/Categories/CategoryButton.jsx
@@ -1,6 +1,6 @@
-import { Button as MuiButton } from '@mui/material';
 import React from 'react';
 import { useMovieList } from '../../context/movieListContext';
+import styled from '@emotion/styled';
 
 export const CategoryButton = ({ label, categoryId }) => {
   const { selectedCategoryId, setSelectedCategoryId } = useMovieList();
@@ -16,27 +16,24 @@ export const CategoryButton = ({ label, categoryId }) => {
   const isActive = selectedCategoryId === categoryId;
 
   return (
-    <MuiButton
-      sx={{
-        height: '3rem',
-        fontWeight: 500,
-        fontSize: '16px',
-        lineHeight: '10px',
-        color: isActive ? '#003E2F' : '#00FFC2',
-        border: '2px solid #00FFC2',
-        borderRadius: '50px',
-        padding: '14px 18px',
-        backgroundColor: isActive ? '#00FFC2' : '#003E2F',
-        textTransform: 'none',
-        margin: '10px 6px',
-        '&:hover': {
-          backgroundColor: isActive ? '#00FFC2' : '#FFF',
-        },
-      }}
-      type="button"
-      onClick={handleClick}
-    >
+    <Button type="button" onClick={handleClick}>
       {label}
-    </MuiButton>
+    </Button>
   );
 };
+
+const Button = styled.button`
+height: '3rem',
+fontWeight: 500,
+fontSize: '16px',
+lineHeight: '10px',
+color: isActive ? '#003E2F' : '#00FFC2',
+border: '2px solid #00FFC2',
+borderRadius: '50px',
+padding: '14px 18px',
+backgroundColor: isActive ? '#00FFC2' : '#003E2F',
+textTransform: 'none',
+margin: '10px 6px',
+'&:hover': {
+  backgroundColor: isActive ? '#00FFC2' : '#FFF',
+`;
