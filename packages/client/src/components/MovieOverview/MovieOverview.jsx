1b91b879601c92ba69b82d62f1d1cdb262991d29
VioMNE <violeta.pavetic@gmail.com>
2023-05-22 13:16:51 +0200

    style/ style solution for components of movie page list

--- packages/client/src/components/MovieOverview/MovieOverview.jsx
+++ packages/client/src/components/MovieOverview/MovieOverview.jsx
@@ -1,4 +1,3 @@
-import { useTheme } from '@mui/material/styles';
 import { MovieListProvider } from '../../context/movieListContext';
 import { Box, Container } from '@mui/material';
 import { Categories } from '../Categories/Categories';
@@ -6,58 +5,51 @@ import { SearchBar } from '../SearchBar/SearchBar';
 import { CustomSelect } from '../Select/CustomSelect';
 import { MovieList } from './MovieList';
 import React from 'react';
+import styled from '@emotion/styled';
+
+const MovieOverviewContainer = styled(Container)`
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+`;
+
+const FlexContainer = styled(Box)`
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  margin-bottom: 4rem;
+
+  ${({ theme }) => theme.breakpoints.down('sm')} {
+    flex-direction: column;
+    align-items: flex-end;
+  }
+`;
+
+const SearchSelectContainer = styled(Box)`
+  display: flex;
+  align-items: flex-end;
+  flex-direction: column;
+
+  ${({ theme }) => theme.breakpoints.down('sm')} {
+    width: 100%;
+    align-items: flex-start;
+    margin-bottom: 3rem;
+  }
+`;
 
 export const MovieOverview = ({ isFavouritePage }) => {
-  const theme = useTheme();
-
   return (
     <MovieListProvider isFavoritePage={isFavouritePage}>
-      <Container
-        sx={{
-          pt: '3rem',
-          pb: '3rem',
-          [theme.breakpoints.up('sm')]: {
-            paddingLeft: '6rem',
-            paddingRight: '7rem',
-          },
-          backgroundImage:
-            'linear-gradient(to right, #000000 0%, #003c2d 50%), linear-gradient(to bottom right, #003c2d 50%, #00ffc2 100%)',
-          backgroundSize: 'cover',
-        }}
-        maxWidth="xl"
-      >
-        <Box
-          sx={{
-            width: '100%',
-            display: 'flex',
-            alignItems: 'flex-start',
-            justifyContent: 'space-between',
-            marginBottom: '4rem',
-            [theme.breakpoints.down('sm')]: {
-              flexDirection: 'column',
-              alignItems: 'flex-end',
-            },
-          }}
-        >
+      <MovieOverviewContainer maxWidth="xl">
+        <FlexContainer>
           <Categories />
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'flex-end',
-              flexDirection: 'column',
-              [theme.breakpoints.down('sm')]: {
-                width: '100%',
-                alignItems: 'flex-start',
-                marginBottom: '3rem',
-              },
-            }}
-          >
+          <SearchSelectContainer>
             <SearchBar />
             <CustomSelect />
-          </Box>
-        </Box>
+          </SearchSelectContainer>
+        </FlexContainer>
         <MovieList />
-      </Container>
+      </MovieOverviewContainer>
     </MovieListProvider>
   );
 };
--- packages/client/src/components/SearchBar/SearchBar.jsx
+++ packages/client/src/components/SearchBar/SearchBar.jsx
@@ -2,20 +2,24 @@ import { Search as SearchIcon } from '@mui/icons-material';
 import { InputAdornment, TextField } from '@mui/material';
 import React from 'react';
 import { useMovieList } from '../../context/movieListContext';
+import styled from '@emotion/styled';
+
+const StyledTextField = styled(TextField)`
+  background: #dbdbdb;
+  color: #dbdbdb;
+  width: 258px;
+  height: 51.75px;
+  border-radius: 37.5px;
+  & fieldset {
+    border: none;
+  }
+`;
 
 export const SearchBar = () => {
   const { searchText, setSearchText } = useMovieList();
 
   return (
-    <TextField
-      sx={{
-        background: '#DBDBDB',
-        color: '#DBDBDB',
-        width: '258px',
-        height: '51.75px',
-        borderRadius: '37.5px',
-        '& fieldset': { border: 'none' },
-      }}
+    <StyledTextField
       placeholder="Search..."
       value={searchText}
       onChange={(e) => {
